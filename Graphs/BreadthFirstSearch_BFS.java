93dd0027429fb6f44073473b0d7392cb83f207e3
premborate24 <137317040+premborate24@users.noreply.github.com>
2023-10-01 00:14:24 +0530

    Update BreadthFirstSearch_BFS.java
    breadth

--- Graphs/BreadthFirstSearch_BFS.java
+++ Graphs/BreadthFirstSearch_BFS.java
@@ -81,3 +81,74 @@ public class BreadthFirstSearch_BFS {
  * Output:
  * 0 1 2 3 4
  */
+
+
+// How does BFS work?
+// Starting from the root, all the nodes at a particular level are visited first and then the nodes of the next level are traversed till all the nodes are visited.
+
+// To do this a queue is used. All the adjacent unvisited nodes of the current level are pushed into the queue and the nodes of the current level are marked visited and popped from the queue.
+
+// Illustration:
+
+// Let us understand the working of the algorithm with the help of the following example.
+
+// Step1: Initially queue and visited arrays are empty.
+
+
+// Queue and visited arrays are empty initially.
+
+// Step2: Push node 0 into queue and mark it visited.
+
+// Push node 0 into queue and mark it visited.
+// Push node 0 into queue and mark it visited.
+
+// Step 3: Remove node 0 from the front of queue and visit the unvisited neighbours and push them into queue.
+
+// Remove node 0 from the front of queue and visited the unvisited neighbours and push into queue.
+// Remove node 0 from the front of queue and visited the unvisited neighbours and push into queue.
+
+// Step 4: Remove node 1 from the front of queue and visit the unvisited neighbours and push them into queue.
+
+// Remove node 1 from the front of queue and visited the unvisited neighbours and push
+// Remove node 1 from the front of queue and visited the unvisited neighbours and push
+
+// Step 5: Remove node 2 from the front of queue and visit the unvisited neighbours and push them into queue.
+
+// Remove node 2 from the front of queue and visit the unvisited neighbours and push them into queue.
+// Remove node 2 from the front of queue and visit the unvisited neighbours and push them into queue.
+
+// Step 6: Remove node 3 from the front of queue and visit the unvisited neighbours and push them into queue. 
+// As we can see that every neighbours of node 3 is visited, so move to the next node that are in the front of the queue.
+
+// Remove node 3 from the front of queue and visit the unvisited neighbours and push them into queue. 
+// Remove node 3 from the front of queue and visit the unvisited neighbours and push them into queue. 
+
+// Steps 7: Remove node 4 from the front of queue and visit the unvisited neighbours and push them into queue. 
+// As we can see that every neighbours of node 4 are visited, so move to the next node that is in the front of the queue.
+
+// Remove node 4 from the front of queue and and visit the unvisited neighbours and push ithem into queue. 
+// Remove node 4 from the front of queue and visit the unvisited neighbours and push them into queue.
+
+// Now, Queue becomes empty, So, terminate these process of iteration.
+
+// Implementation of BFS for Graph using Adjacency List:
+
+// #include <stdbool.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+ 
+// #define MAX_VERTICES 50
+ 
+// // This struct represents a directed graph using
+// // adjacency list representation
+// typedef struct Graph_t {
+ 
+//     // No. of vertices
+//     int V;
+//     bool adj[MAX_VERTICES][MAX_VERTICES];
+// } Graph;
+ 
+// // Constructor
+// Graph* Graph_create(int V)
+// {
+//     Graph* g = malloc(sizeof(Graph));
